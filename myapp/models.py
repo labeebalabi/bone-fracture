5146da3166303f23938f193642020e9a248f61e1
labeeba <labeebalabi78@gmail.com>
2023-05-22 12:49:53 +0530

    it is first commit . it is my pg project bone fracture detection

--- myapp/models.py
+++ myapp/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
- photo = models.ImageField(upload_to="myimage")
+ photo = models.ImageField(upload_to="myimage",default="\media\myimage\oimage.png")
+ 
  date = models.DateTimeField(auto_now_add=True)
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 from .forms import ImageForm
 from .models import Image
 # Create your views here.
 
+
+@never_cache
 def home(request):
  if request.method == "POST":
   form = ImageForm(request.POST, request.FILES)
   if form.is_valid():
    form.save()
  form = ImageForm()
- img = Image.objects.all()
+ img = Image.objects.last()
  return render(request, 'myapp/home.html', {'img':img, 'form':form})
